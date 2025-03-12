c2e966e83753c38e7d2b894be00e883c9ca68031
Salim Saley <limsaley@gmail.com>
2025-03-12 02:49:10 +0000

    enhaNced README

--- ticketsystem/deployment-files/init-scripts/init-user.sql
+++ ticketsystem/deployment-files/init-scripts/init-user.sql
@@ -1,8 +1,4 @@
--- Script à placer dans le dossier ./init-scripts
 -- Sera exécuté au démarrage du conteneur Oracle
-
--- Connexion en tant qu'utilisateur SYS (administrateur)
--- CONNECT SYS/password123@//localhost:1521/XE AS SYSDBA;
 
 alter system set LOCAL_LISTENER='(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))' scope=both;
 alter system register;
@@ -13,16 +9,12 @@ alter system register;
 ALTER SESSION SET CONTAINER = xepdb1;
 
 -- Création de l'utilisateur pour l'application
-CREATE USER '&DB_USERNAME' IDENTIFIED BY '&DB_PASSWORD';
+CREATE USER ticket_user IDENTIFIED BY ticket_password;
 
 -- Attribution des droits nécessaires
 GRANT CONNECT, RESOURCE, DBA TO ticket_user;
 GRANT CREATE SESSION, CREATE TABLE, CREATE VIEW, CREATE SEQUENCE TO ticket_user;
 GRANT UNLIMITED TABLESPACE TO ticket_user;
-
--- Création du schéma (optionnel, peut être fait par JPA/Hibernate)
--- On peut ajouter ici des instructions SQL pour créer des tables, des séquences, etc.
--- ou laisser Hibernate s'en charger avec spring.jpa.hibernate.ddl-auto=update
 
 -- Script de vérification de santé pour le healthcheck
 --CREATE OR REPLACE PROCEDURE check_db_health AS
